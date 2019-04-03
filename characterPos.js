ed8e2dd4c7498115848dce4fee79025a11ea1cf8
Kathleen Pearce <kathleenpearce@live.com>
2019-04-03 13:39:42 -0700

    solution to previous related assingment

---
+++ characterPos.js
@@ -0,0 +1,14 @@
+var letterCounting = {};
+
+function countLetters (str) {
+  str = str.replace(/ /g,'')
+ for (var i = 0; i < str.length; i++ ) {
+   if (!letterCounting[str[i]]) {
+     letterCounting[str[i]] = 1;
+   } else {
+     letterCounting[str[i]] += 1;
+   }
+ }
+ console.log(letterCounting);
+}
+countLetters("Lighthouse in the house")
